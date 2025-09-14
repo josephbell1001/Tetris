053dbb4babe204a2ab59640a74e6f644db72423f
Joseph Bell <josephbell1001@gmail.com>
2025-09-14 01:56:36 -0700

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,32 @@
 #include <raylib.h>
-#include "ball.h"
+#include "grid.h"
+#include "blocks.cpp"
 
 int main() 
 {
-    const Color darkGreen = {20, 160, 133, 255};
-    
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-    
-    while (!WindowShouldClose())
+    Color darkBlue = {44, 44, 127, 255};
+    InitWindow(300, 600, "raylib Tetris");
+    SetTargetFPS(60);          // Set our game to run at 60 frames-per-second, else it'll differ computer to computer
+
+    Grid grid = Grid();
+    grid.Print();
+
+    TBlock block = TBlock();
+
+    while (WindowShouldClose() == false)
+    // checks if ESC key is pressed or if the window is closed, close game
+    // (Hover over functions for more details)
     {
-        ball.Update();
-        
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+        ClearBackground(darkBlue);
+        grid.Draw();
+        block.Draw();
+        
         EndDrawing();
+        /* code */
     }
     
+
     CloseWindow();
+    // return 0;
 }
