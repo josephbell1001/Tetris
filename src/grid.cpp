f8110a34689f09873a906cb948cf26c292bca9d0
Joseph Bell <josephbell1001@gmail.com>
2025-09-14 02:00:30 -0700

    Initial commit

---
+++ src/grid.cpp
@@ -0,0 +1,41 @@
+#include "grid.h"
+#include <iostream>
+#include "colors.h"
+
+Grid::Grid() {
+    //constructor for grid class
+    numRows = 20;
+    numCols = 10;
+    cellSize = 30;
+    Initialize();
+    colors = getCellColors();
+}
+
+void Grid::Initialize() {
+    for (int row = 0; row < numRows; row++) {
+
+        for (int column = 0; column < numCols; column++) {
+
+            grid[row][column] = 0;
+        }
+    }
+}   
+
+void Grid::Print() {
+    for (int row = 0; row < numRows; row++) {
+        for (int column = 0; column < numCols; column++) {
+            std::cout << grid[row][column] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Grid::Draw() {
+    for (int row = 0; row < numRows; row++) {
+        for (int column = 0; column < numCols; column++) {
+            int cellValue = grid[row][column];
+
+            DrawRectangle(column * cellSize+1, row * cellSize+1, cellSize-1, cellSize-1, colors[cellValue]);
+        }
+    }
+}
