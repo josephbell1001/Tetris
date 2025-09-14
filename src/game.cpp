b0154cd5c44b8d3cfbb1b826a9ce824414ea26c3
Joseph Bell <josephbell1001@gmail.com>
2025-09-14 02:53:56 -0700

    Add functinality for block interactions, blocks cannot merge and if a block takes up space, other blocks cannot take up that space

--- src/game.cpp
+++ src/game.cpp
@@ -52,14 +52,14 @@ void Game::HandleInput() {
 
 void Game::MoveBlockLeft() {
     currentBlock.Move(0,-1);
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || BlockFits() == false) {
         currentBlock.Move(0,1);
     }
 }
 
 void Game::MoveBlockRight() {
     currentBlock.Move(0,1);
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || BlockFits() == false) {
         currentBlock.Move(0,-1);
     }
 }
@@ -67,8 +67,9 @@ void Game::MoveBlockRight() {
 
 void Game::MoveBlockDown() {
     currentBlock.Move(1,0);
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || BlockFits() == false) {
         currentBlock.Move(-1,0);
+        LockBlock();
     } 
 }
 
@@ -84,7 +85,26 @@ bool Game::IsBlockOutside() {
 
 void Game::RotateBlock() {
     currentBlock.Rotate();
-    if (IsBlockOutside()) {
+    if (IsBlockOutside() || BlockFits() == false) {
         currentBlock.UndoRotation();
     }
+}
+
+void Game::LockBlock() {
+    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    for (Position item: tiles) {
+        grid.grid[item.row][item.column] = currentBlock.id;
+    }
+    currentBlock = nextBlock;
+    nextBlock = GetRandomBlock();
+}
+
+bool Game::BlockFits() {
+    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    for (Position item: tiles) {
+        if (grid.IsCellEmpty(item.row, item.column) == false) {
+            return false;
+        }
+    }
+    return true;
 }
